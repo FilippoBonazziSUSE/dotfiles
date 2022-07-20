c1b3acab9a23899e009b1208131c5b8cb896a854
Filippo Bonazzi <filippo.bonazzi@suse.com>
2022-07-20 17:32:53 +0200

    Fix VPN toggle script

--- private_dot_config/waybar/helpers/toggle_vpn.sh
+++ private_dot_config/waybar/helpers/toggle_vpn.sh
@@ -1,19 +1,21 @@
 #! /bin/bash
 #
-# Toggle the VPN with the provided ID on and off
+# Toggle the VPN connection ON or OFF
 
+# Provide a VPN name to activate [optional]
+# If no VPN name is provided, the first available one will be selected
 VPN=$1
 
-if [ -z "$VPN" ]
-then
-  exit 1
-fi
-
-ACTIVE=$(nmcli con show --active | grep "$VPN")
+ACTIVE=$(nmcli connection show --active | grep "vpn" | sed 's/\s\+/\t/g' | cut -f1)
 
 if [ -z "$ACTIVE" ]
 then
-  nmcli con up id "$VPN"
+  if [ -z "$VPN" ]
+  then
+    expect ~/.config/waybar/helpers/vpn.exp
+  else
+    expect ~/.config/waybar/helpers/vpn.exp "$VPN"
+  fi
 else
-  nmcli con down id "$VPN"
+  nmcli con down id "$ACTIVE"
 fi
