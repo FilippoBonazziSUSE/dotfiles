59b8ec081287815d9f1544a99465e129ade384ec
Filippo Bonazzi <filippo.bonazzi@suse.com>
2022-08-23 19:56:08 +0200

    Add fwupd widget

---
+++ private_dot_config/waybar/helpers/fwupd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Report the number of available fwupd updates in i3blocks style
+
+fwupdmgr refresh &>/dev/null
+fwupdmgr get-updates &>/dev/null
+
+if [[ "$?" -eq "0" ]]
+then
+  echo -e "\nFirmware updates available"
+  exit 0
+else
+  echo -e "\nNo firmware updates available"
+  exit 1
+fi
