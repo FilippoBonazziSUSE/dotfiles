61050f5213465453f05164bbb0693372faeb9276
Filippo Bonazzi <filippo.bonazzi@suse.com>
2022-08-23 18:21:47 +0200

    Improve zypper, add NPM bar widgets

---
+++ private_dot_config/waybar/helpers/npm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Report the number of available NPM updates in i3blocks style
+
+num_updates=$(npm outdated -g | tail -n +2 | wc -l)
+
+if [[ "$num_updates" -eq "0" ]]
+then
+  exit 1
+else
+  echo -e "$num_updates\n$num_updates packages to update"
+  exit 0
+fi
---
+++ private_dot_config/waybar/helpers/zypper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Report the number of available zypper updates in i3blocks style
+
+num_updates=$(zypper -n lu | grep -c 'v |')
+
+if [[ "$num_updates" -eq "0" ]]
+then
+  exit 1
+else
+  echo -e "$num_updates\n$num_updates packages to update"
+  exit 0
+fi
