4dfc687ad0a4c5ac576491d75cd5ab739aaa2c6c
Filippo Bonazzi <filippo.bonazzi@suse.com>
2024-05-07 10:54:27 +0200

    Fix number of available flatpak updates

--- private_dot_config/waybar/helpers/flatpak.sh
+++ private_dot_config/waybar/helpers/flatpak.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Report the number of available flatpak updates in i3blocks style
 
-num_updates=$(flatpak remote-ls --updates --all | tail -n +2 | wc -l)
+# Obtain the number of updates as the number of lines in the command output,
+# minus the headings line. If no updates are present, the command output will
+# be empty.
+num_updates=$(flatpak remote-ls --updates --all | tail -n +1 | wc -l)
 
 if [[ "$num_updates" -eq "0" ]]
 then
