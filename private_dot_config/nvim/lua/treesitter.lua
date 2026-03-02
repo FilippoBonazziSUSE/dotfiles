f4f63d8b1121c4f6dd072391d9a4589a5f964d0d
Filippo Bonazzi <filippo.bonazzi@suse.com>
2026-03-02 12:03:09 +0100

    nvim: fix nvim-treesitter configuration

--- private_dot_config/nvim/lua/treesitter.lua
+++ private_dot_config/nvim/lua/treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require'nvim-treesitter'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
   ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
